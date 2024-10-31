6231426025182d91e64fa5e5f2db2f25756b30ed
Pedro Paulo Cunha <pedropaulocunha.dev@gmail.com>
2024-10-30 22:05:04 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import poweredImage from './assets/powered.svg'
+import { GridItem } from "./components/GridItem";
 
-function App() {
+import {levels, calculateIMC} from './helpers/imc';
+
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0)
+  const [weightField, setWeightField] = useState<number>(0)
+  
+  const handleCalculateButton = () => {
+    if(heightField && weightField) {
+
+    } else {
+      window.alert("digite todos os campos")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="logo" width={150} height={100} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+        <h1>Calcule o seu IMC</h1>
+        <p>IMC (Índice de Massa Corporal) é uma medida usada para avaliar se uma pessoa está dentro do peso ideal, considerando a altura e o peso. Ele ajuda a identificar possíveis condições de peso inadequado, como baixo peso, sobrepeso ou obesidade.</p>
+        <input 
+          type="number"
+          placeholder="Digite sua altura Ex: 1.5 (em metros)"
+          value={heightField > 0 ? heightField: ''}
+          onChange={e =>setHeightField(parseFloat(e.target.value.replace(',', '.')))}
+        />
+        <input 
+          type="number"
+          placeholder="Digite seu peso Ex: 75.8 (em Kg)"
+          value={weightField > 0 ? weightField: ''}
+          onChange={e =>setWeightField(parseFloat(e.target.value.replace(',', '.')))}
+        />
+        <button
+          onClick={handleCalculateButton}
+        >Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          <div className={styles.grid}>
+            {levels.map((item, key) => (
+              <GridItem key={key} item={item}/>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/components/GridItem/GridItem.tsx
@@ -0,0 +1,19 @@
+import { Level } from "../../helpers/imc";
+import styles from './GridItem.module.css'
+import upImage from '../../assets/up.png'
+import downImage from '../../assets/down.png'
+type Props = {
+    item: Level;
+}
+
+export const GridItem = ({item}: Props) => {
+    return (
+        <div className={styles.main} style={{ backgroundColor: item.color}}>
+            <div className={styles.gridIcon}>
+                <img src={item.icon === 'up' ? upImage : downImage} width={30} />
+            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+            <div className={styles.gridInfo}></div>
+        </div>
+    )
+}
